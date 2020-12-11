6408fc4c57103d1c900fb0ed8486fc0621e9bb0a
IKLOA <fandor1q@yandex.com>
2020-12-11 16:24:56 +0300

    feat: added webpack, eslint, prettier, babel, precommithook and created react structure

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './app.sass'
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>COVID19 Dashboard</h1>
+      </div>
+    )
+  }
+}
+
+export default App
