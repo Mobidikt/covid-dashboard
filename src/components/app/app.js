4663d750fa5d490c183f7a48e19f09c8c9bb0daa
Grenzen <grenzentur@rambler.ru>
2020-12-14 14:10:47 +0300

    fix: add an example of using CovidService class in app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,22 @@ import CovidService from '../../service/covid-service'
 class App extends Component {
   covidService = new CovidService()
 
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    global: null,
+  }
+
+  componentDidMount() {
+    this.covidService.getGlobalCases().then((global) => {
+      this.setState({
+        global,
+      })
+    })
+  }
+
   render() {
+    const { global } = this.state
+    console.log(global)
     // console.log(this.covidService.getListOfCountriesWithFlags())
     // console.log(this.covidService.getActualDate())
     // console.log(this.covidService.getListOfCountries())
