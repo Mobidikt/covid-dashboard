e79fcdf9fc53b3f85af0756fbfbb9dcf64200f67
Grenzen <grenzentur@rambler.ru>
2020-12-14 12:36:14 +0300

    feat: add class covid-service and babel plugins for it

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import './app.sass'
 
+import CovidService from '../../service/covid-service'
+
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  covidService = new CovidService()
+
   render() {
+    // console.log(this.covidService.getListOfCountriesWithFlags())
+    // console.log(this.covidService.getActualDate())
+    // console.log(this.covidService.getListOfCountries())
+    // console.log(this.covidService.getGlobalCases())
     return (
       <div>
         <h1>COVID19 Dashboard</h1>
